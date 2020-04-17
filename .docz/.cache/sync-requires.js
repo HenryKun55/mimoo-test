8ee4eb414ad64952eb05d48d92ce414ce59635b3
HenryKun55 <henriqueflavio55@gmail.com>
2020-04-17 00:05:00 -0300

    First commit

---
+++ .docz/.cache/sync-requires.js
@@ -0,0 +1,15 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/henry/Documentos/WorkSpace/mimoo/.docz/.cache/dev-404-page.js"))),
+  "component---readme-md": hot(preferDefault(require("/home/henry/Documentos/WorkSpace/mimoo/README.md"))),
+  "component---src-components-alert-mdx": hot(preferDefault(require("/home/henry/Documentos/WorkSpace/mimoo/src/components/Alert.mdx"))),
+  "component---src-components-button-mdx": hot(preferDefault(require("/home/henry/Documentos/WorkSpace/mimoo/src/components/Button.mdx"))),
+  "component---src-index-mdx": hot(preferDefault(require("/home/henry/Documentos/WorkSpace/mimoo/src/index.mdx"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/home/henry/Documentos/WorkSpace/mimoo/.docz/src/pages/404.js")))
+}
+
