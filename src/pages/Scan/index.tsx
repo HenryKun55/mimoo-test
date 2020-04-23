2d28edc5763fbbd93aea0f19305294eaf5c3facc
HenryKun55 <henriqueflavio55@gmail.com>
2020-04-23 04:39:41 -0300

    Video Scan Constraints

--- src/pages/Scan/index.tsx
+++ src/pages/Scan/index.tsx
@@ -1,17 +1,29 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef, RefObject } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { IoMdArrowRoundBack } from 'react-icons/io'
+import Quagga from 'quagga'
 
-import { Container, Header, Title, Camera, ConfirmButton } from './styles'
 import { colors } from '../../styles'
-import { useDispatch, useSelector } from 'react-redux'
-
 import * as ProductActions from '../../store/ducks/products/actions'
 import { ApplicationState } from '../../store'
+import {
+  Container,
+  Header,
+  Footer,
+  Title,
+  ConfirmButton,
+  BarcodeReader,
+  CodeContainer,
+  TitleBarCode
+} from './styles'
+
+// import './drawingBuffer.css'
 
 const Scan: React.FC = () => {
-
-  const [barcode, setBarcode] = useState('0123456789')
+  const barcodeReaderRef = useRef<HTMLDivElement>(null)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const [barcode, setBarcode] = useState('')
 
   const { product, loading } = useSelector((state: ApplicationState) => state.products)
 
@@ -19,17 +31,95 @@ const Scan: React.FC = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
+    if (!canvasRef.current) {
+      return
+    }
+    const canvas: HTMLCanvasElement = canvasRef.current
+    draw(canvas)
+  }, [canvasRef.current])
+
+  useEffect(() => {
     if(product) {
       history.push('/product/confirmation')
     }
   }, [loading, product])
+
+  useEffect(() => {
+    if (barcodeReaderRef.current !== null) {
+      initQuagga()
+    }
+  }, [barcodeReaderRef.current])
+
+  function initQuagga() {
+    Quagga.init({
+      inputStream : {
+        name : "Live",
+        type : "LiveStream",
+        target: barcodeReaderRef.current,
+        constraints: {
+          // width: '5%',
+          // height: '100%',
+          facingMode: "environment",
+        },
+      },
+      decoder : {
+        readers : ["ean_reader"],
+      }
+    }, handleBarcodeReaderInit)
+  }
+
+  function handleBarcodeReaderInit(err) {
+    if (err) return
+
+    Quagga.start()
+    Quagga.onDetected(handleBarcodeDetected)
+    // Quagga.onProcessed(handleBarcodeProcessed)
+  }
+
+  function handleBarcodeDetected(result) {
+    console.log(result)
+    setBarcode(result.codeResult.code)
+  }
+
+  function draw(canvas: HTMLCanvasElement) {
+    const ctx = canvas.getContext("2d")
+
+    if(ctx) {
+      ctx.beginPath()
+
+      ctx.moveTo(0, 0)
+      ctx.lineTo(ctx.canvas.width, 0)
+      ctx.lineTo(ctx.canvas.width, ctx.canvas.width)
+      ctx.lineTo(0, ctx.canvas.width)
+      ctx.lineTo(0, 0)
+      ctx.closePath()
+  
+      const horizontalSize = ctx.canvas.width * 0.1
+      const verticalSize = ctx.canvas.height * 0.4
+
+      const width = ctx.canvas.width * 0.9
+      const height = ctx.canvas.height * 0.7
+
+      ctx.moveTo(horizontalSize, verticalSize)
+      ctx.lineTo(horizontalSize, height)
+      ctx.lineTo(width, height)
+      ctx.lineTo(width, verticalSize)
+
+      ctx.closePath()
+  
+      ctx.fillStyle = "rgba(0,0,0,.5)"
+      ctx.lineWidth = 1
+      ctx.fill()
+      ctx.stroke()
+    }
+  }
 
   function handleBack() {
     history.goBack()
   }
 
   function handleConfirm() {
-    if(barcode.length > 9) {
+    if(barcode.length > 0) {
       dispatch(ProductActions.getProductRequest(barcode))
     }
   }
@@ -40,9 +130,16 @@ const Scan: React.FC = () => {
       <IoMdArrowRoundBack color={colors.primary} size={30} onClick={handleBack}/>
         <Title>Escanear Produto</Title>
       </Header>
-      <Camera />
+      <BarcodeReader ref={barcodeReaderRef}>
+        <TitleBarCode>Escaneando o código <br /> de barras</TitleBarCode>
+        <canvas ref={canvasRef} id='meu-canvas'/>
+      </BarcodeReader>
+      {/* <CodeContainer>
 
-      <ConfirmButton variant="secondary" onClick={handleConfirm}>Confirmar</ConfirmButton>
+      </CodeContainer> */}
+      <Footer>
+        <ConfirmButton variant="secondary" onClick={handleConfirm}>Confirmar</ConfirmButton>
+      </Footer>
     </Container>
   )
 }
--- src/pages/Scan/styles.ts
+++ src/pages/Scan/styles.ts
@@ -5,6 +5,7 @@ import { colors } from '../../styles'
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  overflow: hidden;
   height: 100vh;
 `
 
@@ -12,7 +13,6 @@ export const Header = styled.div`
   display: flex;
   padding: 20px;
 `
-
 
 export const Title = styled.h1`
   flex: 1;
@@ -23,9 +23,54 @@ export const Title = styled.h1`
   opacity: 0.47;
 `
 
-export const Camera = styled.div`
-  flex: 1;
-  background-color: ${colors.greyLight};
+export const TitleBarCode = styled.h1`
+  top: 16%;
+  z-index: 1;
+  font-size: 23px;
+  position: absolute;
+  text-align: center;
+  color: ${colors.secondary};
+  font-family: 'Circular Std Book';
 `
 
-export const ConfirmButton = styled(Button)``
+export const BarcodeReader = styled.div`
+  display: flex;
+  justify-content: center;
+  flex: 1;
+
+  video {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+  }
+
+  canvas {
+    width: 100%;
+    height: 75%;
+    position: absolute;
+  }
+  
+  .drawingBuffer {
+    display: none;
+  }
+
+`
+
+export const CodeContainer = styled.div`
+  background: #FFFFFF 0% 0% no-repeat padding-box;
+  border-radius: 4px;
+  opacity: 1;
+  backdrop-filter: blur(30px);
+  -webkit-backdrop-filter: blur(30px);
+`
+
+export const Footer = styled.div`
+  display: flex;  
+  height: 47px;
+`
+
+export const ConfirmButton = styled(Button)`
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+`
