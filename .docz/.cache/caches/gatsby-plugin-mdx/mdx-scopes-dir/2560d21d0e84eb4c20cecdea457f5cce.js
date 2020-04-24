accad1fbec624a2bfaa118cd2a2296d0c02029bb
HenryKun55 <henriqueflavio55@gmail.com>
2020-04-24 16:07:13 -0300

    Remove Docz

--- .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/2560d21d0e84eb4c20cecdea457f5cce.js
+++
@@ -1,9 +0,0 @@
-import { Playground, Props } from "docz";
-import ListBrands from "../../../../../src/components/ListBrands";
-import * as React from 'react';
-export default {
-  Playground,
-  Props,
-  ListBrands,
-  React
-};
--- .docz/doczrc.js
+++
@@ -1,9 +0,0 @@
-export default {
-  typescript: true,
-  title: 'Mimoo',
-  menu: [
-    'Getting Started', // auto ordered menu "Introduction"
-  ],
-  files: ['**/*.mdx'],
-  ignore: ['README.md'],
-}
